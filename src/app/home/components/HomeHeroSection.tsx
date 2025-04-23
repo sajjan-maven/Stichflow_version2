84e7e10dbbdeb077b8a6e931d067788e90d91a1a
sangeethaMavencart <154861833+sangeethaMavencart@users.noreply.github.com>
2025-04-23 16:51:10 +0530

    build error fixed

--- src/app/home/components/HomeHeroSection.tsx
+++ src/app/home/components/HomeHeroSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
 import React, {useState} from "react";
 
 const appIcons = [
